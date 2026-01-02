fabe2b7b30c564f8904333ea2373360dce17ca95
Pavel Toman <paqowo@gmail.com>
2026-01-02 22:30:31 +0100

    Fix Vite entry for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from '../App';
 import './index.css';
 
 const rootElement = document.getElementById('root');
@@ -13,7 +12,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
